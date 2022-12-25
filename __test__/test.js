5b029b4976300d7a2d085021f0654eca9391c430
Omar <omardraz94@gmail.com>
2022-12-25 16:15:27 -0500

    figuring out the first few things

--- __test__/test.js
+++ __test__/test.js
@@ -1,3 +1,45 @@
-import { render, screen } from "@testing-library/react";
 import React from "react";
-import App from "../src/App";
+import ReactDOM from "react-dom/client";
+import { act } from "react-dom/test-utils";
+import { EventViewHero } from "../src/EventView/EventViewHero.js";
+
+describe("Event View Hero", () => {
+  const event = {
+    name: "Event #2",
+    attendees: {
+      host: "Omar",
+      guests: ["Kareem", "Reem", "Ahmed"],
+    },
+    location: "Markham",
+  };
+
+  // create a container
+  let container;
+
+  // reset the container
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  it("renders an event form", () => {
+    // Step 1 - create a component to host that data
+    const component = <EventViewHero event={event} />;
+    // Step 2 - render that data in the DOM
+    act(() => {
+      ReactDOM.createRoot(container).render(component);
+    });
+    // Step 3 - expect the data
+    expect(document.body.textContent).toContain("Event #1");
+  });
+  it("renders an event form with a different name", () => {
+    const component = <EventViewHero event={event} />;
+
+    act(() => {
+      ReactDOM.createRoot(container).render(component);
+    });
+
+    expect(document.body.textContent).toContain("Event #2");
+  });
+  it("has a photo showcasing the event", () => {});
+});
---
+++ src/EventView/EventViewHero.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const EventViewHero = ({ event }) => {
+  return <div>{event.name}</div>;
+};
